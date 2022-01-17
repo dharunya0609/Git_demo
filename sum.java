a5db52c22c0286622c28e337b5d5278312383146
dharunya0609 <dharunyaramesh800@gmail.com>
2022-01-17 21:38:24 +0530

    Included Subtraction tooo...

--- sum.java
+++ sum.java
@@ -6,6 +6,9 @@ public class sum
     int a=10,b=10;
     int c=a+b;
     System.out.println("Sum of a and b is :"+c);
+    int d=a-b;
+    System.out.println("Subtraction of a and b is :"+d);
+
   }
 }
 
