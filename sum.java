d26254440b162344dbefa2cd59b1f431688bef8f
dharunya0609 <dharunyaramesh800@gmail.com>
2022-01-17 21:32:13 +0530

    PGM for addition of 2 numbers in java

---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class sum
+{
+ public static void main(String args[])
+  {
+    int a=10,b=10;
+    int c=a+b;
+    System.out.println("Sum of a and b is :"+c);
+  }
+}
+
